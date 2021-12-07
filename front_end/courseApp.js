44515d54786e4eae61b8d95e76385d8f3dcf0383
jadedeo <jade.deo18@stjohns.edu>
2021-12-07 12:56:48 -0500

    undo all option

--- front_end/courseApp.js
+++ front_end/courseApp.js
@@ -20,16 +20,10 @@ var updateView = async(button) => {
     }
 
     if(numQueries == 0){
-        api = `api/`;
-
-        //alert("Please enter a query.");
+        alert("Please enter a query.");
     }
     else if(numQueries > 2){
         alert("Please enter only a max of 2 queries.");
-        
-        const data = await fetch(api);
-        const model = await data.json();
-        renderView(model);
     }
     else if(numQueries == 2){
         //console.log("combined query");
